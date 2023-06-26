c1d0f7481d8ccd2c826ee034f3fdcda6d60de5a2
Ben Funk <70977039+Coopsims@users.noreply.github.com>
2023-06-26 13:41:54 -0600

    Added Folders for cleanliness

--- DataSummary/GenerateSummary.py
+++ DataSummary/GenerateSummary.py
@@ -57,7 +57,7 @@ def main():
             fsummary = an.summarize(df=myData, N_sensors=16, partitionSpots=myData.testSpots)
             summary = pd.concat([summary, fsummary], ignore_index=True)
 
-    summary_fname = os.path.join('/Users/benfunk/DataspellProjects/MethaneDataScience/Output', 'Test.csv')
+    summary_fname = os.path.join('/Output', 'Test.csv')
     summary.to_csv(summary_fname)
 
 if __name__ == '__main__':
