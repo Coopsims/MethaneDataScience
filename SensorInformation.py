17efb3442deef6be829cadca1ad2fe7d0dbdc50e
Ben Funk <70977039+Coopsims@users.noreply.github.com>
2023-06-26 13:31:14 -0600

    Created Summary code

---
+++ SensorInformation.py
@@ -0,0 +1,5 @@
+class sensor():
+    def __init__(self, fileName, sensorNum):
+        self.parentFile = fileName
+        self.sensor = sensorNum
+        # self.sensorID TODO: WILL BE ADDED IN FUTURE UPDATE
